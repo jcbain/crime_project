d921eb6d4300f54ac39e8fb451a5131bfcdb99bd
James Bain <jamescbain@gmail.com>
2016-03-16 15:12:52 -0500

    create histograms for the different categories of crime

--- data_exploration.R
+++ data_exploration.R
@@ -1,4 +1,8 @@
 setwd(dir='Desktop/Spring 2016/CS 7001/Project_1/part_2/')
 
 df13<-read.csv('output_data/new_2013.csv')
-hist(df13$Crimes_Against_Persons)
+
+# histograms of super-types of crime
+hist(df13$Crimes_Against_Persons/df13$Total_Offenses)
+hist(df13$Crimes_Against_Property/df13$Total_Offenses)
+hist(df13$Crimes_Against_Society/df13$Total_Offenses)
