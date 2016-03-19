eb919fed36601378b0d6e87801d283c7209d55fa
James Bain <jamescbain@gmail.com>
2016-03-19 15:44:06 -0500

    run a linear model

--- data_exploration.R
+++ data_exploration.R
@@ -4,23 +4,27 @@ library(ggplot2)
 library(plotly)
 library(reshape2)
 
-# read in a year of the data
+#######################
+## READ IN THE FILES ##
+#######################
+
+## read in a year of the data ##
 df12<-read.csv('output_data/new_2012.csv')
 df13<-read.csv('output_data/new_2013.csv')
 df14<-read.csv('output_data/new_2014.csv')
 
 
-#create state codes
+## create state codes ##
 df12$codes<-as.factor(state.abb[match(df12$State,state.name)] )
 df13$codes<-as.factor(state.abb[match(df13$State,state.name)] )
 df14$codes<-as.factor(state.abb[match(df14$State,state.name)] )
 
-# aggregate some data based on the sum of offenses per each state
+## aggregate some data based on the sum of offenses per each state ##
 states12<-aggregate(cbind(Population, Total_Offenses,Crimes_Against_Persons,Crimes_Against_Property,Crimes_Against_Society)~codes, data=df12, sum, na.rm=TRUE)
 states13<-aggregate(cbind(Population, Total_Offenses,Crimes_Against_Persons,Crimes_Against_Property,Crimes_Against_Society)~codes, data=df13, sum, na.rm=TRUE)
 states14<-aggregate(cbind(Population, Total_Offenses,Crimes_Against_Persons,Crimes_Against_Property,Crimes_Against_Society)~codes, data=df14, sum, na.rm=TRUE)
 
-#find proportions based off of population for each state
+## find proportions based off of population for each state ##
 proStates12<-states12[,3:ncol(states12)]/states12$Population
 prost12<-cbind(states12[,1:2],proStates12)
 
@@ -31,21 +35,26 @@ proStates14<-states14[,3:ncol(states14)]/states14$Population
 prost14<-cbind(states14[,1:2],proStates14)
 
 
-# transform counts into proportions (each offense/total population per 100,000 people)
-props12<-df12[6:64]/df12$Population # first find the proportion for each crime
+## transform counts into proportions (each offense/total population per 100,000 people)
+props12<-df12[6:64]/df12$Population 
 prop12<-cbind(df12[,c(1:5,ncol(df12)-1,ncol(df12))],props12)
 
-props13<-df13[, 6:68]/(df13$Population) # first find the proportion for each crime
+props13<-df13[, 6:68]/(df13$Population) 
 prop13<-cbind(df13[,c(1:5,69:70)],props13)
 
-props14<-df14[, 6:68]/(df14$Population) # first find the proportion for each crime
+props14<-df14[, 6:68]/(df14$Population) 
 prop14<-cbind(df14[,c(1:5,69:70)],props14)
 
+## count the number of NAs in a data frame ##
+na_count <-sapply(prop12, function(y) sum(length(which(is.na(y)))))
+na_count <- data.frame(na_count)
 
+
+model<-lm(Population ~Negligent_Manslaughter,data = prop14)
 
 # play around with some graphs to explore the data
-plot_ly(prop13, x = Population, y = Crimes_Against_Persons, text = paste("Agency: ", Agency_Name),
-        mode = "markers", color = Agency_Type)
+plot_ly(prop12, x = Crimes_Against_Persons, y = Population, text = paste("Agency: ", Agency_Name),
+        mode = "markers", color = codes)
 
 plot_ly(prop13, x = Total_Offenses, color = Region, type = "box")
 plot_ly(prop13, x = Region,y = Homicide_Offenses, color = State, type = "box") %>%
